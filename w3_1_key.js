f6b6bdd39d5e03cd6b1f69cc3cabe0e681083bd6
kabita kumari <74282784+manvimahi20@users.noreply.github.com>
2021-08-13 19:34:49 +0530

    Add files via upload

--- w3_1_key.js
+++ w3_1_key.js
@@ -6,12 +6,12 @@
 // rollno : 12 };
 // Sample Output: name,sclass,rollno
 
-var student = {
-    name : "David Rayy",
-    sclass : "VI",
-    rollno : 12 };
-for(var x in student){
-    console.log(x)
-}
+// var student = {
+//     name : "David Rayy",
+//     sclass : "VI",
+//     rollno : 12 };
+// for(var x in student){
+//     console.log(x)
+// }
 
 
