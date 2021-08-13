83db5391e382818d9ef7cffa8c19c3b173dfed8c
kabita kumari <74282784+manvimahi20@users.noreply.github.com>
2021-08-13 19:35:57 +0530

    Add files via upload

---
+++ class_construction.js
@@ -0,0 +1,32 @@
+class person{
+    constructor (){
+        this.name="manvi",
+        this.age=18,
+        this.sarname="mahi"
+
+    }
+    sum(){
+        let a=12
+        let b=13
+        let c=a+b
+        console.log(c)
+        console.log(this.name)
+    }
+}
+var new_person=new person()
+console.log(new_person)
+new_person.sum()
+
+
+
+// class Car {
+//     constructor(name, year) {
+//       this.name = "manvi";
+//       this.year = 18;
+//     }
+//     name(params) {
+        
+//     }
+//   }
+// let new_car=new Car()
+// console.log(new_car)  
