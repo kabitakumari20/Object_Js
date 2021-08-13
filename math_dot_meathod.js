309fff0f77c73640de263c87800702687e0c2b4e
kabita kumari <74282784+manvimahi20@users.noreply.github.com>
2021-08-13 19:37:30 +0530

    Add files via upload

---
+++ math_dot_meathod.js
@@ -0,0 +1,45 @@
+// min number with sprade oprater without sprade oprate we can no do----------------
+// var arr=[2,4,7,9,1,0]
+// console.log(Math.min(...arr))
+
+
+
+// max number---------------------------------
+// const array1 = [1, 3, 2];
+
+// console.log(Math.max(...array1));
+
+
+// squrt of number----------------------------
+
+// a=100
+// console.log(Math.sqrt(a))
+
+
+// Number of floor-------------------------
+
+// let b=4.9
+// console.log(Math.floor(b)); 
+
+
+// Math.trunc(x) returns the integer part of :
+// var a=4.9
+// Math.trunc(a);
+
+// Math.pow(x, y) returns the value of x to the power of y:
+// var a=8
+// var b=2
+// console.log(Math.pow(a,b)); 
+
+
+// Math.abs(x) returns the absolute (positive) value of x:
+// var a =-4.7
+// console.log(Math.abs(a));
+
+
+let a=null
+console.log(a)
+
+
+
+
