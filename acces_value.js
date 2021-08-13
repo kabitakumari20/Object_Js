e9816c6c5dc2bd84e8e2371d2f3010c8e36c4b67
kabita kumari <74282784+manvimahi20@users.noreply.github.com>
2021-08-13 19:40:47 +0530

    Add files via upload

---
+++ acces_value.js
@@ -0,0 +1,38 @@
+// var myCar = {
+//     make: 'Ford',
+//     model: 'Mustang',
+//     year: 1969
+// };
+
+// console.log(myCar.make)
+// var sat=myCar.make="manvi"
+// var sat=myCar.name="manvi"
+// console.log(sat)
+// console.log(myCar)
+
+
+
+// // 2 way of access object
+// var myCar = {
+//     "make": 'Ford',
+//     "model": 'Mustang',
+//     year: 1969
+// };
+// console.log(myCar["make"])
+
+
+// 3 way of access object
+
+var myCar = {
+    make: 'Ford',
+    model: 'Mustang',
+    year: 1969
+};
+let {model} = myCar
+console.log(model)
+
+
+
+
+
+
